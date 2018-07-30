6cd1637d4821662208db60167e2902a89fb8325c
Manan Vaghasiya <manan@impactanalytics.co>
2018-07-30 17:02:38 +0530

    add styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import { default as styled } from 'styled-components';
+
+const Test = styled.h1`
+  color: yellow;
+`;
 
 export default class HelloWorld extends React.Component<any, any> {
   render() {
     return (
       <div style={{ color: this.props.color }}>
-        Hello world
+        <Test>
+            Hello world
+        </Test>
       </div>
     );
   }
